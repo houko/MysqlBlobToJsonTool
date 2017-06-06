697d8c614af7e8c3b7a1ba5da9c9282e99153db6
xiaomo <xiaomo@xiaomo.info>
2017-06-06 16:16:14 +0800

    init

---
+++ src/main/java/info/xiaomo/mysqlBlobToJsonTool/AppMain.java
@@ -0,0 +1,18 @@
+package info.xiaomo.mysqlBlobToJsonTool;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiamoo.info
+ * QQ    : 83387856
+ * Date  : 17/6/6 16:14
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class AppMain {
+}
