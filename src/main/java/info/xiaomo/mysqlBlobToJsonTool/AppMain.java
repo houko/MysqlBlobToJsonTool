7f216417f6c5f5cc57d7c48de7953a7643e00324
xiaomo <xiaomo@xiaomo.info>
2017-06-09 09:26:42 +0800

    java fx初始化环境

--- src/main/java/info/xiaomo/mysqlBlobToJsonTool/AppMain.java
+++ src/main/java/info/xiaomo/mysqlBlobToJsonTool/AppMain.java
@@ -1,5 +1,11 @@
 package info.xiaomo.mysqlBlobToJsonTool;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  * いま 最高の表現 として 明日最新の始発．．～
@@ -14,5 +20,19 @@ package info.xiaomo.mysqlBlobToJsonTool;
  * desc  :
  * Copyright(©) 2017 by xiaomo.
  */
-public class AppMain {
+public class AppMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/mainScene.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
