9a1fe8afce92a1d9df23b08d6070f99d94223212
xiaomo <83387856@qq.com>
2017-06-12 00:24:27 +0800

    spring boot

---
+++ api/src/main/java/info/xiaomo/api/ApiMain.java
@@ -0,0 +1,99 @@
+package info.xiaomo.api;
+
+import io.swagger.annotations.ApiOperation;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+/**
+ * @author 小莫 (https://xiaomo.info) (https://github.com/syoubaku)
+ * @version : 2017/1/11 16:34
+ */
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("info.xiaomo")
+@RestController
+@EnableSwagger2
+public class ApiMain {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ApiMain.class, args);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ApiIgnore()
+    @ApiOperation(value = "重定向到api首页")
+    public ModelAndView index() {
+        return new ModelAndView("redirect:/swagger-ui.html");
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.bzuol.app"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("mysqlBlobToJsonTool")
+                .description("mysqlBlobToJsonTool")
+                .termsOfServiceUrl("http://api.xiaomo.info")
+                .contact("小莫")
+                .version("1.0")
+                .build();
+    }
+}
