6bea1aac21471563b86614acb9adf92b4f0fd96c
xiaomo <xiaomo@xiaomo.info>
2017-06-23 17:04:47 +0800

    jdbc

--- src/main/java/info/xiaomo/api/controller/TablesController.java
+++ src/main/java/info/xiaomo/api/controller/TablesController.java
@@ -1,16 +1,5 @@
 package info.xiaomo.api.controller;
 
-import info.xiaomo.api.service.TableService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  * いま 最高の表現 として 明日最新の始発．．～
@@ -25,23 +14,8 @@ import java.util.List;
  * desc  :
  * Copyright(©) 2017 by xiaomo.
  */
-@Api
-@RestController
-@RequestMapping("/table")
+
 public class TablesController {
 
-    private final
-    TableService tableService;
 
-    @Autowired
-    public TablesController(TableService tableService) {
-        this.tableService = tableService;
-    }
-
-    @ApiOperation(value = "查询所有表", notes = "查询所有表", httpMethod = "GET", produces = MediaType
-            .APPLICATION_JSON_UTF8_VALUE)
-    @RequestMapping("/queryTables")
-    public List<String> queryTables() {
-        return tableService.queryTable();
-    }
 }
--- src/main/java/info/xiaomo/api/db/JdbcUtil.java
+++ src/main/java/info/xiaomo/api/db/JdbcUtil.java
@@ -1,5 +1,9 @@
 package info.xiaomo.api.db;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  * いま 最高の表現 として 明日最新の始発．．～
@@ -15,4 +19,107 @@ package info.xiaomo.api.db;
  * Copyright(©) 2017 by xiaomo.
  */
 public class JdbcUtil {
+
+    private Connection conn = null;
+    private Statement st = null;
+    private ResultSet rs = null;
+
+    public JdbcUtil(String databaseName, String ip, String userName, String password) {
+        try {
+            //写入驱动所在处，打开驱动
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+//            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            //数据库，用户，密码，创建与具体数据库的连接
+            conn = DriverManager.getConnection("jdbc:mysql://" + ip + ":3306/" + databaseName, userName, password);
+            //创建执行sql语句的对象
+            st = conn.createStatement();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String query(String sqlStatement, int n) {
+        String result = "";
+        try {
+            rs = st.executeQuery(sqlStatement);
+            while (rs != null && rs.next()) {
+                result = rs.getString(n);
+                //列的记数是从1开始的，这个适配器和C#的不同
+            }
+
+            if (rs != null) {
+                rs.close();
+            }
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public List<Object> queryList(String sql) {
+        PreparedStatement pstmt;
+        List<Object> ret = new ArrayList<>();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                ret.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+
+    public List<String> queryTables(String dbName) {
+        PreparedStatement pstmt;
+        List<String> ret = new ArrayList<>();
+
+        try {
+            String sql = "Select Table_Name From Information_Schema.Tables Where Table_Schema = '" + dbName + "'";
+            System.out.println(sql);
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+            while (rs.next()) {
+                ret.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ret;
+    }
+
+    public int query(String sqlStatement) {
+        int row = 0;
+        try {
+            row = st.executeUpdate(sqlStatement);
+            this.close();
+            return row;
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.close();
+            return row;
+        }
+    }
+
+    public void close() {
+        try {
+            if (rs != null)
+                this.rs.close();
+            if (st != null)
+                this.st.close();
+            if (conn != null)
+                this.conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
