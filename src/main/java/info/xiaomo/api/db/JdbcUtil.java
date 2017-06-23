ea1736dffc655b06eaa33cbcaf4748133d1fa797
xiaomo <xiaomo@xiaomo.info>
2017-06-23 17:02:40 +0800

    jdbc

---
+++ src/main/java/info/xiaomo/api/db/JdbcUtil.java
@@ -0,0 +1,18 @@
+package info.xiaomo.api.db;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 17/6/23 17:02
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class JdbcUtil {
+}
