0fdb48749a6d7855a05d1552cd651853083337e5
xiaomo <xiaomo@xiaomo.info>
2017-07-28 18:10:15 +0800

    线上环境

--- src/main/java/info/xiaomo/api/AppMain.java
+++ src/main/java/info/xiaomo/api/AppMain.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 把今天最好的表现当作明天最新的起点．．～
@@ -26,15 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan("info.xiaomo")
-@RestController
 public class AppMain {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(AppMain.class, args);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "启动成功...";
-    }
 
 }
--- src/main/resources/web/src/config.ts
+++ src/main/resources/web/src/config.ts
@@ -1,13 +1,12 @@
-import { environment } from './environments/environment';
 // export const domainName = environment.production?'https://api.tusoapp.com':'https://api.dev.tusoapp.com';
-const domainName="http://localhost:9999";
+const domainName = 'http://localhost:9999';
 
 export const login = {
-    login:domainName+'/login',
-    logout:domainName+'/logout',
-}
+    login: domainName + '/login',
+    logout: domainName + '/logout',
+};
 export const list = {
-    all:domainName+'/queryTables',
-    listByTable:domainName+'/queryDataList',
-    listByTableMore:domainName+'/queryData',
-}
+    all: domainName + '/queryTables',
+    listByTable: domainName + '/queryDataList',
+    listByTableMore: domainName + '/queryData',
+};
